778240b71823f372aded85e21b590b4bdba502d1
Pervertcoder <lance60702@gmail.com>
2026-01-13 13:21:51 +0800

    1/13 fields css setting

--- static/booking.js
+++ static/booking.js
@@ -144,7 +144,53 @@ TPDirect.setupSDK(
   "sandbox"
 );
 
-// TPDirect.card.setup();
+let fields = {
+  number: {
+    element: "#card-number",
+    placeholder: "**** **** **** ****",
+  },
+  expirationDate: {
+    element: "#card-expiration-date",
+    placeholder: "MM/YY",
+  },
+  ccv: {
+    element: "#card-ccv",
+    placeholder: "ccv",
+  },
+};
+TPDirect.card.setup({
+  fields: fields,
+  styles: {
+    // Style all elements
+    input: {
+      color: "gray",
+      border: "1px solid #e8e8e8e8",
+    },
+    // style valid state
+    ".valid": {
+      color: "green",
+    },
+    // style invalid state
+    ".invalid": {
+      color: "red",
+    },
+    // Media queries
+    // Note that these apply to the iframe, not the root window.
+    "@media screen and (max-width: 400px)": {
+      input: {
+        color: "orange",
+        border: "1px solid #e8e8e8e8",
+        borderRadius: "5px",
+      },
+    },
+  },
+  // 此設定會顯示卡號輸入正確後，會顯示前六後四碼信用卡卡號
+  isMaskCreditCardNumber: true,
+  maskCreditCardNumberRange: {
+    beginIndex: 6,
+    endIndex: 11,
+  },
+});
 
 const comfirmBtn = document.querySelector(".comfirmBtn");
 comfirmBtn.addEventListener("click", () => {
